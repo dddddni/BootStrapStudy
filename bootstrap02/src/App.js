71fbe0f914fccd6687ee96e4fce188f9dac99553
dddddni <tjgus9812@gmail.com>
2021-01-06 15:02:41 +0900

    쇼핑몰 생성 -> visual studio core로 내용 이전함.

--- bootstrap02/src/App.js
+++ bootstrap02/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Content from './Content';
-import 'bootstrap/dist/css/bootstrap.css';
+import Header from './Main/Header';
+import Footer from './Main/Footer';
+import Content from './Main/Content';
 
 class App extends Component{
   constructor(props){
     super(props);
+
 }
   render(){
     return (
--- bootstrap02/src/Main/Header.js
+++ bootstrap02/src/Main/Header.js
@@ -6,9 +6,9 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 class Header extends Component {
     render() {
         return (
-            <div style = {{fontFamily:'Noto Sans KR'}, {fontSize:'13px'}}>
+            <div className = 'Header'>
                 <Navbar bg="light" expand="lg">
-                    {/* <Navbar.Brand href="#home">테스트</Navbar.Brand> */}
+                    <Navbar.Brand href="#home">테스트</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
@@ -21,17 +21,17 @@ class Header extends Component {
                             <Nav.Link href="#link7">SHOES/BAG</Nav.Link>
                             <Nav.Link href="#link8">ACC</Nav.Link>
                             <Nav.Link href="#link9">SALE</Nav.Link>
-                            <NavDropdown title="마이페이지" id="basic-nav-dropdown" className ="test">
-                                <NavDropdown.Item href="#action/1.1" className = "test2">관심상품</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/1.2" className = "test2">주문조회</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/1.3" className = "test2">적립금</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/1.4" className = "test2">마이쿠폰</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/1.5" className = "test2">내가쓴글</NavDropdown.Item>
+                            <NavDropdown title="마이페이지" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/1.1" className = 'item01'>관심상품</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/1.2" className = 'item01'>주문조회</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/1.3" className = 'item01'>적립금</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/1.4" className = 'item01'>마이쿠폰</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/1.5" className = 'item01'>내가쓴글</NavDropdown.Item>
                             </NavDropdown>
-                            <NavDropdown title="커뮤니티" id="basic-nav-dropdown" className ="test">
-                                <NavDropdown.Item href="#action/3.1" className = "test2">NOTICE</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3" className = "test2">Q & A</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2" className = "test2">REVIEW</NavDropdown.Item>
+                            <NavDropdown title="커뮤니티" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1" className = 'item01'>NOTICE</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3" className = 'item01'>Q & A</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2" className = 'item01'>REVIEW</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                         <Nav>
